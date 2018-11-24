b97dbd0f4e75a71968d88b2c7096b1ad346d224a
David <David@Davi-PC>
2018-11-24 23:28:13 +0100

    Creamos los Geters y Seters para los atributos de la clase Posicion.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -1,5 +1,23 @@
 package org.iesalandalus.programacion.caballoajedrez;
 
 public class Posicion {
-
+	private int fila;
+	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
+	public void setColumna(char columna) {
+		this.columna = columna;
+	}
+	
 }
